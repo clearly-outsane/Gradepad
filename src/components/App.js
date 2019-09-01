86fe1e8fd477a2d20f79dc5b609ce49ae0234171
Rohan Mayya <rohan.mayya@gmail.com>
2019-09-01 23:37:25 +0530

    setup prettierrc config, state for create class

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import CreateClass from './Class/CreateClass'
 
 class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <nav></nav>
-        <Router>
-          <Route path="/create-class" component={CreateClass} />
-        </Router>
-      </Fragment>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <nav></nav>
+                <Router>
+                    <Route path="/create-class" component={CreateClass} />
+                </Router>
+            </Fragment>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/components/Class/CreateClass.js
+++ src/components/Class/CreateClass.js
@@ -1,25 +1,37 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react'
 
 class CreateClass extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>
-          <form>
-            <label>
-              Class Name:
-              <input />
-            </label>
-            <br />
-            <label>
-              Course:
-              <input />
-            </label>
-          </form>
-        </div>
-      </Fragment>
-    );
-  }
+    state = {
+        className: '',
+        course: ''
+    }
+
+    render() {
+        console.log(this.state.className)
+        return (
+            <Fragment>
+                <div>
+                    <form>
+                        <label>
+                            Class Name:
+                            <input
+                                name="class"
+                                value={this.state.className}
+                                onChange={className =>
+                                    this.setState({ className })
+                                }
+                            />
+                        </label>
+                        <br />
+                        <label>
+                            Course:
+                            <input />
+                        </label>
+                    </form>
+                </div>
+            </Fragment>
+        )
+    }
 }
 
-export default CreateClass;
+export default CreateClass
