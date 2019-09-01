78db1510976d09dc310eb81ea5c29f1454a49ac4
Rohan Mayya <rohan.mayya@gmail.com>
2019-09-01 21:10:04 +0530

    added react-router-dom

---
+++ src/components/Class/CreateClass.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from "react";
+
+class CreateClass extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <p>hey</p>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default CreateClass;
